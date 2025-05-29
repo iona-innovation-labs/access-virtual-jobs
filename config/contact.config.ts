8869965b4cf9fed3a525591330413addad757d00
Kobe Brian Santos <kobe0santos0@gmail.com>
2025-05-30 02:02:45 +0800

    feat: created configs

---
+++ config/contact.config.ts
@@ -0,0 +1,8 @@
+export const contactConfig = {
+  title: "Let's Start a Conversation",
+  badge: "Get in touch",
+  description:
+    "We're here to help you find the perfect job for you. Let's talk about your needs and how we can assist you.",
+  email: "support@accessvirtualstaffing.com",
+  office: "105 S. Narcissus Ave. Suite 512 West Palm Beach, FL 33401",
+};
