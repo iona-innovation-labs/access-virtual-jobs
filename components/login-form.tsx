3845e7587400f88d6de45c15a60a6a81cfbf1e8f
Steven Gragg <sgragg5619@gmail.com>
2025-06-16 20:48:11 +0800

    fix: update login description to reflect Google account only

--- components/login-form.tsx
+++ components/login-form.tsx
@@ -133,9 +133,7 @@ export default function LoginPage() {
         <Card>
           <CardHeader className="text-center">
             <CardTitle className="text-xl">Welcome back</CardTitle>
-            <CardDescription>
-              Login with your Apple or Google account
-            </CardDescription>
+            <CardDescription>Login with your Google account</CardDescription>
           </CardHeader>
           <CardContent>
             <div className="grid gap-6">
