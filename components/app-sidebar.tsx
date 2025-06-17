2a2715ea2cb0a9e8b73d25bc1f42939ebc05e947
Steven Gragg <sgragg5619@gmail.com>
2025-06-18 07:54:23 +0800

    fix: update dashboard link to point to overview page

--- components/app-sidebar.tsx
+++ components/app-sidebar.tsx
@@ -73,7 +73,7 @@ export function AppSidebar({ ...props }: React.ComponentProps<typeof Sidebar>) {
               asChild
               className="data-[slot=sidebar-menu-button]:!p-1.5"
             >
-              <Link href={"/dashboard"}>
+              <Link href={"/overview"}>
                 <Logo size="md" />
                 <span className="text-base font-semibold">AVJ</span>
               </Link>
